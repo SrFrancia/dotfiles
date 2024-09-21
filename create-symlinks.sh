41778c44dd6f29ada5a95f458c3d2405d764a078
SrFrancia <23404787+SrFrancia@users.noreply.github.com>
2024-09-21 21:33:56 +0100

    Modified so symlinks are created individually

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
+readonly DIRNAME=dotfiles
 
 function linkDotFile {
   file="$1"
   if [ ! -e ~/$file -a ! -L ~/$file ]; then
       yecho "$file not found, linking..." >&2
-      ln -s ~/dotfiles/$file ~/$file
+      ln -s ~/$DIRNAME/$file ~/$file
   else
       gecho "$file found, ignoring..." >&2
   fi
 }
 
-for file in $( ls -A .[a-zA-z]* | sed "s/[\&\;\|$$$$$$\{\}\$\`\~\!\@\#\%\^\*\+\=\-\/\:\;\"\'\ ]/\\\&/g" ); do
-    linkDotFile "'$file'"
-done
+linkDotFile .bashrc
+linkDotFile .bash_aliases
+linkDotFile .config/nvim/init.lua
+linkDotFile .bashrc
+
+# for file in $( ls -A .[a-zA-z]* | sed "s/[\&\;\|$$$$$$\{\}\$\`\~\!\@\#\%\^\*\+\=\-\/\:\;\"\'\ ]/\\\&/g" ); do
+#     linkDotFile "'$file'"
+# done
