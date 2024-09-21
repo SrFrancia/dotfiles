f7a60c474ddb35cc11c49694cd71fe3120a43117
SrFrancia <23404787+SrFrancia@users.noreply.github.com>
2024-09-21 16:08:28 +0100

    Fix typo with clangd removed rubygem

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-apt install curl gcc make clandg python3 python3-venv python-is-python3 pip lua5.4 luarocks rubygem ripgrep fd-find
+apt install curl gcc make clangd python3 python3-venv python-is-python3 pip lua5.4 luarocks ripgrep fd-find
 python3 -m pip install --user --upgrade pynvim
