7f73ce4a3b6ae11f56b89777ec0a9b5e0c6c0fc8
SrFrancia <23404787+SrFrancia@users.noreply.github.com>
2024-09-21 16:02:46 +0100

    Pynvim instalation

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-apt install curl gcc make clandg python3 python3-venv python-is-python3 lua5.4 luarocks rubygem ripgrep fd-find
+apt install curl gcc make clandg python3 python3-venv python-is-python3 pip lua5.4 luarocks rubygem ripgrep fd-find
+python3 -m pip install --user --upgrade pynvim
