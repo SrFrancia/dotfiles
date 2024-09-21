70a9a9cd971f362b45c610da5fec23fd66379e7a
SrFrancia <23404787+SrFrancia@users.noreply.github.com>
2024-09-21 16:19:06 +0100

    Install pynvim through apt and not pip

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt update
-apt install curl gcc make clangd python3 python3-venv python-is-python3 pip lua5.4 luarocks ripgrep fd-find
-python3 -m pip install --user --upgrade pynvim
+apt install curl gcc make clangd python3 python3-venv python-is-python3 pip python3-pynvim lua5.4 luarocks ripgrep fd-find
+# python3-pip install --user --upgrade pynvim ## replaced with apt install python3-nvim
