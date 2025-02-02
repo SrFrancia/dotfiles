49a10c3a67d35a10b6302c6ae1f60e8115cf7a33
SrFrancia <23404787+SrFrancia@users.noreply.github.com>
2025-02-02 19:32:29 +0000

    mc: autoinstall and typo

--- install-requirements.sh
+++ install-requirements.sh
@@ -6,11 +6,11 @@ if [ "$EUID" -ne 0 ]
 fi
 
 apt update
-apt install git curl gcc make clangd python3 python3-venv python-is-python3 pip python3-pynvim lua5.4 luarocks ripgrep fd-find
+apt install git curl gcc make clangd python3 python3-venv python-is-python3 pip python3-pynvim lua5.4 luarocks ripgrep fd-find -y
 # python3-pip install --user --upgrade pynvim ## replaced with apt install python3-nvim
 
 # Install JetBrainsMono Nerd Font
 if [[ ! -e /usr/local/share/fonts/JetBrainsMonoNerdFont-Regular.ttf ]]; then
-  curl -s -L -o JerBrainsMono.tar.xz https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.tar.xz
+  curl -s -L -o JetBrainsMono.tar.xz https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.tar.xz
   tar -xf JetBrainsMono.tar.xz -C /usr/local/share/fonts
 fi
