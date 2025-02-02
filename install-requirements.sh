283492a4e2fad4a329daf804837b8dc1d6040438
SrFrancia <23404787+SrFrancia@users.noreply.github.com>
2025-02-02 23:45:30 +0000

    Check nvim installed

--- install-requirements.sh
+++ install-requirements.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+
+if [ "$EUID" -ne 0 ]
+  then echo "Por favor ejecuta como root"
+  exit
+fi
+
 set -e
 set -u
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
+function check_preq {
+  (command -v $1 > /dev/null  && echo "$1 encontrado.") || 
+    (echo "$1 no encontrado, instala antes de seguir." && exit 1)
+}
+
+check_preq nvim
 
 apt update
 apt install git curl gcc make clangd python3 python3-venv python-is-python3 pip python3-pynvim lua5.4 luarocks ripgrep fd-find -y
@@ -14,15 +22,15 @@ apt install git curl gcc make clangd python3 python3-venv python-is-python3 pip 
 
 # Install JetBrainsMono Nerd Font
 if [[ ! -e /usr/local/share/fonts/JetBrainsMonoNerdFont-Regular.ttf ]]; then
-  echo "Downloading JetBrainsMono Nerd Font..."
+  echo "Descargando JetBrainsMono Nerd Font..."
   set +e
   curl --retry 5 --retry-delay 3 -L -o JetBrainsMono.tar.xz https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
-  [[  $? -ne 0 ]] && echo "Failed to download JetBrainsMono Nerd Font!" && exit 1
+  [[  $? -ne 0 ]] && echo "Fallo descargado JetBrainsMono Nerd Font!" && exit 1
   set -e
   if [[ ! -d /usr/local/share/fonts ]]; then
     mkdir -p /usr/local/share/fonts
   fi
   tar -xf JetBrainsMono.tar.xz -C /usr/local/share/fonts
   rm JetBrainsMono.tar.xz
-  echo "JetBrainsMono Nerd Font installed!"
+  echo "JetBrainsMono Nerd Font instalado!"
 fi
